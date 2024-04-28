845811f7008759c67942d1428a6103ec90803c69
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-04-28 21:50:33 +0700

    modified menu page (cashier role)

--- pages/cashier/menu/index.tsx
+++ pages/cashier/menu/index.tsx
@@ -1,11 +1,43 @@
+import { Button } from "@nextui-org/react";
+import { ListNumbers, PlusCircle } from "@phosphor-icons/react";
+
 // components
 import Layout from "@/components/wrapper/SecondaryLayout";
 
 export default function MenuPage() {
   return (
     <Layout title="Menu Kasir">
-      <section className="pt-24">
-        <div className="container">menu cashier page</div>
+      <section className="container flex h-screen items-center justify-center">
+        <div className="grid w-[450px] grid-cols-2 grid-rows-2 gap-x-2 gap-y-6">
+          <Button
+            variant="bordered"
+            size="lg"
+            endContent={
+              <ListNumbers weight="bold" size={24} className="text-cyan-600" />
+            }
+            className="border-cyan-600 font-medium text-cyan-600"
+          >
+            Daftar Transaksi
+          </Button>
+
+          <Button
+            size="lg"
+            endContent={<PlusCircle weight="bold" size={24} />}
+            className="bg-cyan-600 font-medium text-white"
+          >
+            Penjualan
+          </Button>
+
+          <Button
+            variant="flat"
+            color="danger"
+            size="lg"
+            onClick={() => confirm("Apakah anda ingin melakukan penutupan?")}
+            className="col-span-2 font-medium"
+          >
+            Penutupan
+          </Button>
+        </div>
       </section>
     </Layout>
   );
