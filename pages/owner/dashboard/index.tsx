1d47d5ca4742871d616151df663c7de8c4ac54b2
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-04-29 20:36:34 +0700

    add card on the dashboard page (owner role)

--- pages/owner/dashboard/index.tsx
+++ pages/owner/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { Basket, Money } from "@phosphor-icons/react";
+
 // components
 import Container from "@/components/wrapper/DashboardContainer";
 import Layout from "@/components/wrapper/DashboardLayout";
@@ -6,7 +8,48 @@ export default function DashboardPage() {
   return (
     <Layout title="Dashboard Owner">
       <Container>
-        <div>owner dashboard page</div>
+        <section className="grid grid-cols-3 gap-4">
+          <div className="grid h-[150px] w-full rounded-xl bg-white p-6 shadow-[2px_2px_12px_rgba(0,0,0,0.15)]">
+            <div className="inline-flex items-center gap-2">
+              <div className="aspect-square rounded-lg bg-green-100 p-2">
+                <Money weight="bold" size={24} className="text-green-600" />
+              </div>
+              <p className="font-medium text-default-600">Total Keuntungan</p>
+            </div>
+
+            <h6 className="text-default-900-900 justify-self-center text-3xl font-semibold">
+              Rp1.186.917
+            </h6>
+          </div>
+
+          <div className="grid h-[150px] w-full rounded-xl bg-white p-6 shadow-[2px_2px_12px_rgba(0,0,0,0.15)]">
+            <div className="inline-flex items-center gap-2">
+              <div className="aspect-square rounded-lg bg-red-100 p-2">
+                <Money weight="bold" size={24} className="text-red-600" />
+              </div>
+              <p className="font-medium text-default-600">Laba Kotor</p>
+            </div>
+
+            <h6 className="text-default-900-900 justify-self-center text-3xl font-semibold">
+              Rp2.819.109
+            </h6>
+          </div>
+
+          <div className="grid h-[150px] w-full rounded-xl bg-white p-6 shadow-[2px_2px_12px_rgba(0,0,0,0.15)]">
+            <div className="inline-flex items-center gap-2">
+              <div className="aspect-square rounded-lg bg-orange-100 p-2">
+                <Basket weight="bold" size={24} className="text-orange-500" />
+              </div>
+              <p className="font-medium text-default-600">
+                Barang Rusak/Hilang
+              </p>
+            </div>
+
+            <h6 className="text-default-900-900 justify-self-center text-3xl font-semibold">
+              12
+            </h6>
+          </div>
+        </section>
       </Container>
     </Layout>
   );
