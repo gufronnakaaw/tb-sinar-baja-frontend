75a7220e4c8e3f2df283c27c7b99e8ed2e168b97
gufronnakaaw <gufronnakaaw@gmail.com>
2024-04-30 16:17:03 +0700

    implements nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,36 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  function setColor() {
+    const ownerColor = "#006FEE";
+    const adminColor = "#84cc16";
+    const cashierColor = "#f43f5e";
+    const defaultColor = "#000";
+
+    if (router.pathname.startsWith("/owner")) {
+      return ownerColor;
+    }
+
+    if (router.pathname.startsWith("/admin")) {
+      return adminColor;
+    }
+
+    if (router.pathname.startsWith("/cashier")) {
+      return cashierColor;
+    }
+
+    return defaultColor;
+  }
+
   return (
     <NextUIProvider>
+      <NextNProgress color={setColor()} />
       <Component {...pageProps} />
     </NextUIProvider>
   );
