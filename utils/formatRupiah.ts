2dae93d3ee8f3946754bcdf16fe636a8c4a3388c
gufronnakaaw <gufronnakaaw@gmail.com>
2024-05-04 10:39:58 +0700

    modified formatRupiah function

--- utils/formatRupiah.ts
+++ utils/formatRupiah.ts
@@ -1,15 +1,8 @@
-export function formatRupiah(angka: number): string {
-  const number_string = angka.toString();
-  const split = number_string.split(",");
-  const sisa = split[0].length % 3;
-  let rupiah = split[0].substring(0, sisa);
-  const ribuan = split[0].substring(sisa).match(/\d{1,3}/gi);
-
-  if (ribuan) {
-    const separator = sisa ? "." : "";
-    rupiah += separator + ribuan.join(".");
-  }
-
-  rupiah = split[1] !== undefined ? rupiah + "," + split[1] : rupiah;
-  return rupiah;
+export function formatRupiah(price: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
 }
