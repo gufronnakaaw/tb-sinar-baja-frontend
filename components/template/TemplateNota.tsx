530a6c37262b16f1c7dc46959df50288e8b3e8d5
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-05-04 12:17:57 +0700

    modified tempalte-nota components

--- components/template/TemplateNota.tsx
+++ components/template/TemplateNota.tsx
@@ -31,7 +31,7 @@ const transactions: NotaProps[] = [
   },
   {
     id: 2,
-    product_name: "Eka/Aplus Board 120x240x6",
+    product_name: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     amount: "6 lbr",
     price: 110372,
     total: 662250,
@@ -51,21 +51,27 @@ export default function TemplateNota() {
 
     switch (columnKey) {
       case "amount":
-        return <div className="text-default-900">{transaction.amount}</div>;
+        return (
+          <div className="font-semibold text-default-900">
+            {transaction.amount}
+          </div>
+        );
       case "product_name":
         return (
-          <div className="text-default-900">{transaction.product_name}</div>
+          <div className="max-w-[300px] font-semibold text-default-900">
+            {transaction.product_name}
+          </div>
         );
       case "price":
         return (
-          <div className="text-default-900">
-            Rp {formatRupiah(transaction.price)}
+          <div className="font-semibold text-default-900">
+            {formatRupiah(transaction.price)}
           </div>
         );
       case "total":
         return (
-          <div className="text-default-900">
-            Rp {formatRupiah(transaction.total)}
+          <div className="font-semibold text-default-900">
+            {formatRupiah(transaction.total)}
           </div>
         );
 
@@ -75,104 +81,100 @@ export default function TemplateNota() {
   };
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-200 py-[1000px]">
-      {/* template */}
-      <div className="h-[1754px] w-[1240px] bg-white px-24 py-32">
-        <div className="grid gap-16">
-          <h4 className="text-center text-4xl font-bold uppercase text-black">
-            Nota
-          </h4>
+    <div className="grid gap-16 pt-8">
+      <h4 className="text-center text-2xl font-bold uppercase text-black">
+        Nota
+      </h4>
 
-          <div className="grid grid-cols-2 items-start gap-8">
-            <div className="grid gap-1">
-              <div className="flex gap-2 text-default-900">
-                <div className="w-10 font-semibold">No.</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">SB.816824676735</p>
-              </div>
-
-              <div className="flex gap-2 text-default-900">
-                <div className="w-10 font-semibold">Tgl.</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">3 Mei 2024</p>
-              </div>
-
-              <div className="flex gap-2 text-default-900">
-                <div className="w-10 font-semibold">Ket.</div>
-                <div className="font-semibold">:</div>
-                <p className="w-[350px] font-medium uppercase">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-                  est?
-                </p>
-              </div>
-            </div>
-
-            <div className="grid gap-1 justify-self-end">
-              <div className="flex gap-2 text-default-900">
-                <div className="w-[140px] font-semibold">Nama Penerima</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">Umum</p>
-              </div>
-
-              <div className="flex gap-2 text-default-900">
-                <div className="w-[140px] font-semibold">No. Telp</div>
-                <div className="font-semibold">:</div>
-                <p className="w-max font-medium uppercase">0891234567890</p>
-              </div>
-
-              <div className="flex gap-2 text-default-900">
-                <div className="w-[140px] font-semibold">Alamat</div>
-                <div className="font-semibold">:</div>
-                <p className="w-[350px] font-medium uppercase">Jawa Timur</p>
-              </div>
-            </div>
+      <div className="grid grid-cols-2 items-start gap-8">
+        <div className="grid gap-1">
+          <div className="grid grid-cols-[30px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">No</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">SB.816824676735</p>
           </div>
 
-          <div className="grid gap-4">
-            <Table
-              isHeaderSticky
-              aria-label="nota table"
-              selectionMode="single"
-              classNames={customStyleTable}
-            >
-              <TableHeader columns={columns}>
-                {(column) => (
-                  <TableColumn key={column.uid}>{column.name}</TableColumn>
+          <div className="grid grid-cols-[30px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">Tgl</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">3 Mei 2024</p>
+          </div>
+
+          <div className="grid grid-cols-[30px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">Ket</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+              est?
+            </p>
+          </div>
+        </div>
+
+        <div className="grid gap-1">
+          <div className="grid grid-cols-[80px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">Penerima</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">Umum</p>
+          </div>
+
+          <div className="grid grid-cols-[80px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">No. Telp</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">0891234567890</p>
+          </div>
+
+          <div className="grid grid-cols-[80px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="font-semibold">Alamat</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">
+              TB. Sinar Baja - Jl. Letjend Sutoyo No.67, Burengan, Kec.
+              Pesantren, Kabupaten Kediri, Jawa Timur 64131
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid gap-4">
+        <Table
+          isHeaderSticky
+          aria-label="nota table"
+          selectionMode="single"
+          classNames={customStyleTable}
+        >
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={column.uid}>{column.name}</TableColumn>
+            )}
+          </TableHeader>
+
+          <TableBody items={transactions}>
+            {(transaction) => (
+              <TableRow key={transaction.id}>
+                {(columnKey) => (
+                  <TableCell>{renderCell(transaction, columnKey)}</TableCell>
                 )}
-              </TableHeader>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
 
-              <TableBody items={transactions}>
-                {(transaction) => (
-                  <TableRow key={transaction.id}>
-                    {(columnKey) => (
-                      <TableCell>
-                        {renderCell(transaction, columnKey)}
-                      </TableCell>
-                    )}
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
+        <div className="grid gap-1 justify-self-end rounded-xl border-[2px] border-default-200 p-4">
+          <div className="grid grid-cols-[100px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="w-24 font-semibold">Total</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">943.000</p>
+          </div>
 
-            <div className="grid gap-1 justify-self-end rounded-xl border-[2px] border-default-200 p-4">
-              <div className="flex gap-2 text-default-900">
-                <div className="w-24 font-semibold">Total</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">Rp 943.000</p>
-              </div>
+          <div className="grid grid-cols-[100px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="w-24 font-semibold">Ongkir</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">25.000</p>
+          </div>
 
-              <div className="flex gap-2 text-default-900">
-                <div className="w-24 font-semibold">Ongkir</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">Rp 25.000</p>
-              </div>
-
-              <div className="flex gap-2 text-default-900">
-                <div className="w-24 font-semibold">Tagihan</div>
-                <div className="font-semibold">:</div>
-                <p className="font-medium uppercase">Rp 968.000</p>
-              </div>
-            </div>
+          <div className="grid grid-cols-[100px_6px_1fr] gap-1 text-sm text-default-900">
+            <div className="w-24 font-semibold">Tagihan</div>
+            <div className="font-semibold">:</div>
+            <p className="font-semibold">968.000</p>
           </div>
         </div>
       </div>
