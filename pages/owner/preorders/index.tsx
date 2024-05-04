9e01c613a5ded6e5573f86ac223eafc695bb300e
gufronnakaaw <gufronnakaaw@gmail.com>
2024-05-04 14:11:51 +0700

    create pre orders table (owner role)

---
+++ pages/owner/preorders/index.tsx
@@ -0,0 +1,133 @@
+import {
+  Button,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import React from "react";
+
+// components
+import InputSearchBar from "@/components/input/InputSearchBar";
+import Container from "@/components/wrapper/DashboardContainer";
+import Layout from "@/components/wrapper/DashboardLayout";
+
+// utils
+import usePagination from "@/hooks/usepagination";
+import { customStyleTable } from "@/utils/customStyleTable";
+import { formatRupiah } from "@/utils/formatRupiah";
+
+// dummy data
+import { preorders, PreOrdersType } from "@/_dummy/preorders";
+
+export default function PreOrdersPage() {
+  const { page, pages, data, setPage } = usePagination(preorders, 10);
+
+  const columns = [
+    { name: "ID Pre Order", uid: "preorders_id", sortable: false },
+    { name: "Tanggal", uid: "preorders_date", sortable: true },
+    {
+      name: "Nama Pelanggan",
+      uid: "customer",
+      sortable: false,
+    },
+    { name: "Total", uid: "total", sortable: true },
+    { name: "Aksi", uid: "action", sortable: false },
+  ];
+
+  const renderCell = (preorders: PreOrdersType, columnKey: React.Key) => {
+    const cellValue = preorders[columnKey as keyof PreOrdersType];
+
+    switch (columnKey) {
+      case "preorders_id":
+        return <div className="text-default-900">{preorders.id}</div>;
+      case "preorders_date":
+        return <div className="text-default-900">{preorders.date}</div>;
+      case "customer":
+        return <div className="text-default-900">{preorders.customer}</div>;
+      case "total":
+        return (
+          <div className="text-default-900">
+            {formatRupiah(preorders.total)}
+          </div>
+        );
+      case "action":
+        return (
+          <Button
+            variant="bordered"
+            color="default"
+            size="sm"
+            onClick={() => alert(`ID Pre Order: ${preorders.id}`)}
+            className="font-medium"
+          >
+            Detail
+          </Button>
+        );
+
+      default:
+        return cellValue;
+    }
+  };
+
+  return (
+    <Layout title="Pre Order List">
+      <Container className="gap-8">
+        <h4 className="text-lg font-semibold text-default-900">Pre Order</h4>
+
+        <div className="grid gap-4">
+          <div className="flex flex-wrap items-center justify-between gap-4">
+            <InputSearchBar
+              placeholder="Cari pre order..."
+              className="w-full sm:max-w-[500px]"
+            />
+
+            <Button
+              variant="solid"
+              color="primary"
+              className="w-full font-medium sm:w-max"
+            >
+              Buat Pre Order
+            </Button>
+          </div>
+
+          <Table
+            isHeaderSticky
+            aria-label="preorders table"
+            color="primary"
+            selectionMode="single"
+            classNames={customStyleTable}
+          >
+            <TableHeader columns={columns}>
+              {(column) => (
+                <TableColumn key={column.uid}>{column.name}</TableColumn>
+              )}
+            </TableHeader>
+
+            <TableBody items={data}>
+              {(preorders) => (
+                <TableRow key={preorders.id}>
+                  {(columnKey) => (
+                    <TableCell>{renderCell(preorders, columnKey)}</TableCell>
+                  )}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+
+          <Pagination
+            isCompact
+            showControls
+            color="primary"
+            page={page}
+            total={pages}
+            onChange={setPage}
+            className="justify-self-center"
+          />
+        </div>
+      </Container>
+    </Layout>
+  );
+}
