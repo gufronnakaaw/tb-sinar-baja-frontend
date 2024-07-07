469796ace7e39deef290a44072e2e13dd46dca9b
gufronnakaaw <gufronnakaaw@gmail.com>
2024-07-07 14:00:14 +0700

    create component CardSellingCashier

---
+++ components/card/CardSellingCashier.tsx
@@ -0,0 +1,93 @@
+import { Button } from "@nextui-org/react";
+import { ArrowRight } from "@phosphor-icons/react";
+
+// components
+import CustomTooltip from "@/components/tooltip";
+
+// utils
+import { ListProdukCashier } from "@/pages/cashier/menu/selling";
+import { ProdukSearchType } from "@/types/products.type";
+import { formatRupiah } from "@/utils/formatRupiah";
+
+type CardSellingProductProps = {
+  item: ProdukSearchType;
+  setListProduk: React.Dispatch<React.SetStateAction<ListProdukCashier[]>>;
+  field: string | undefined;
+};
+
+export default function CardSellingCashier({
+  setListProduk,
+  field,
+  item,
+}: CardSellingProductProps) {
+  const produk = {
+    kode_item: item.kode_item,
+    nama_produk: !item.nama_produk_asli ? "-" : item.nama_produk_asli,
+    total_stok: !item.total_stok ? 0 : item.total_stok,
+    qty: 1,
+    satuan_kecil: !item.satuan_kecil ? "-" : item.satuan_kecil,
+    harga: item[field as keyof ProdukSearchType],
+    gudang: !item.gudang ? [] : item.gudang,
+    rak: !item.rak ? "-" : item.rak,
+    diskon_langsung_item: 0,
+    diskon_persen_item: 0,
+    total_harga: item[field as keyof ProdukSearchType],
+    subtotal: item[field as keyof ProdukSearchType] * 1,
+    hargaquantity: item.hargaquantity,
+    harga_selected: item[field as keyof ProdukSearchType],
+    gudang_id: !item.gudang ? "" : item.gudang[item.gudang.length - 1].nama,
+  };
+
+  function handleAddProduk() {
+    setListProduk((prev) => {
+      if (prev.find((produk) => produk.kode_item == item.kode_item)) {
+        return [...prev];
+      }
+
+      return [...prev, produk];
+    });
+  }
+  return (
+    <div className="grid grid-cols-[1fr_50px] items-center rounded-xl border border-default-300 p-4 transition hover:border-rose-500">
+      <div className="grid w-3/4 gap-[2px]">
+        <h1 className="font-bold text-default-900">{item.nama_produk_asli}</h1>
+        <div className="grid grid-cols-[110px_6px_1fr] gap-1 text-sm text-black">
+          <div className="font-medium">Harga</div>
+          <div className="font-medium">:</div>
+          <p className="font-bold text-rose-500">
+            {formatRupiah(item[field as keyof ProdukSearchType])}
+          </p>
+        </div>
+        {item.gudang?.map(({ nama, stok }) => {
+          return (
+            <>
+              <div className="grid grid-cols-[110px_6px_1fr] gap-1 text-sm text-black">
+                <div className="font-medium">Stok {nama}</div>
+                <div className="font-medium">:</div>
+                <p className="font-bold text-rose-500">
+                  {stok} {item.satuan_kecil}
+                </p>
+              </div>
+            </>
+          );
+        })}
+      </div>
+
+      <div className="flex items-end justify-between gap-4">
+        {item[field as keyof ProdukSearchType] ? (
+          <CustomTooltip content="Tambahkan">
+            <Button
+              isIconOnly
+              variant="flat"
+              color="danger"
+              size="sm"
+              onClick={handleAddProduk}
+            >
+              <ArrowRight weight="bold" size={20} />
+            </Button>
+          </CustomTooltip>
+        ) : null}
+      </div>
+    </div>
+  );
+}
