c9b81c013c55d5570320bc079707cc62420aa99f
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-08-13 19:54:07 +0700

    fix template preorder component

--- components/template/TemplatePreorder.tsx
+++ components/template/TemplatePreorder.tsx
@@ -1,74 +1,10 @@
-import { FinalDetail, ProdukFinal } from "@/types/preorders.type";
+import { FinalDetail } from "@/types/preorders.type";
 import { formatDateWithoutTime } from "@/utils/formatDate";
 import { formatRupiah } from "@/utils/formatRupiah";
 import { angkaTerbilang } from "@/utils/terbilang";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-} from "@nextui-org/react";
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 
 const Preorder = (props: FinalDetail, ref: any) => {
-  const columns = [
-    { name: "No", uid: "no" },
-    { name: "Kode Pabrik", uid: "kode_pabrik" },
-    { name: "Nama Produk", uid: "nama_produk" },
-    { name: "Qty", uid: "qty" },
-    { name: "Harga", uid: "harga" },
-    { name: "Jumlah", uid: "jumlah" },
-  ];
-
-  const renderCell = (
-    index: number,
-    item: ProdukFinal,
-    columnKey: React.Key,
-  ) => {
-    const cellValue = item[columnKey as keyof ProdukFinal];
-
-    switch (columnKey) {
-      case "no":
-        return (
-          <div className="text-[10px] font-medium text-black">{index}</div>
-        );
-      case "kode_pabrik":
-        return (
-          <div className="text-[10px] font-medium text-black">
-            {!item.kode_pabrik ? "-" : item.kode_pabrik}
-          </div>
-        );
-      case "nama_produk":
-        return (
-          <div className="w-max text-[10px] font-medium text-black">
-            {item.nama_produk}
-          </div>
-        );
-      case "qty":
-        return (
-          <div className="w-max text-[10px] font-medium text-black">
-            {item.qty} {item.satuan}
-          </div>
-        );
-      case "harga":
-        return (
-          <div className="w-max text-[10px] font-medium text-black">
-            {formatRupiah(item.harga)}
-          </div>
-        );
-      case "jumlah":
-        return (
-          <div className="w-max text-[10px] font-medium text-black">
-            {formatRupiah(item.jumlah)}
-          </div>
-        );
-      default:
-        return cellValue;
-    }
-  };
-
   return (
     <>
       <style media="print">
@@ -78,8 +14,9 @@ const Preorder = (props: FinalDetail, ref: any) => {
     "
         }
       </style>
+
       <div className="container grid gap-2 px-8 pt-4 font-inter" ref={ref}>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="flex items-start justify-between gap-4">
           <div className="grid gap-1">
             <h1 className="font-bold text-black">TB. SINAR BAJA</h1>
             <p className="max-w-[300px] text-[10px] font-medium text-black">
@@ -89,6 +26,7 @@ const Preorder = (props: FinalDetail, ref: any) => {
             <p className="max-w-[300px] text-[10px] font-medium text-black">
               082140735711
             </p>
+
             <div className="mt-2">
               <h5 className="text-[10px] font-medium text-black">Kepada</h5>
               <h5 className="text-[10px] font-medium text-black">
@@ -100,96 +38,91 @@ const Preorder = (props: FinalDetail, ref: any) => {
             </div>
           </div>
 
-          <div className="grid justify-end">
-            <div className="flex flex-col">
-              <h1 className="font-bold uppercase text-black">PURCHASE ORDER</h1>
-              <div className="grid h-3">
-                <div className="grid">
-                  <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
-                    <div className="font-medium">Nomor</div>
-                    <div className="font-medium">:</div>
-                    <p className="font-medium">{props.id_preorder}</p>
-                  </div>
+          <div className="grid items-start gap-1">
+            <h1 className="font-bold uppercase text-black">PURCHASE ORDER</h1>
 
-                  <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
-                    <div className="font-medium">Tanggal</div>
-                    <div className="font-medium">:</div>
-                    <p className="font-medium">
-                      {formatDateWithoutTime(props.created_at)}
-                    </p>
-                  </div>
+            <div className="grid">
+              <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
+                <div className="font-medium">Nomor</div>
+                <div className="font-medium">:</div>
+                <p className="font-medium">{props.id_preorder}</p>
+              </div>
 
-                  <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
-                    <div className="font-medium">ID Supplier</div>
-                    <div className="font-medium">:</div>
-                    <p className="font-medium">
-                      {!props.supplier_id ? "-" : props.supplier_id}
-                    </p>
-                  </div>
-                  <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
-                    <div className="font-medium">Email</div>
-                    <div className="font-medium">:</div>
-                    <p className="font-medium">{props.email_supplier}</p>
-                  </div>
-                  <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
-                    <div className="font-medium">No. Telpon</div>
-                    <div className="font-medium">:</div>
-                    <p className="font-medium">{props.no_telp}</p>
-                  </div>
-                </div>
+              <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
+                <div className="font-medium">Tanggal</div>
+                <div className="font-medium">:</div>
+                <p className="font-medium">
+                  {formatDateWithoutTime(props.created_at)}
+                </p>
+              </div>
+
+              <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
+                <div className="font-medium">ID Supplier</div>
+                <div className="font-medium">:</div>
+                <p className="font-medium">
+                  {!props.supplier_id ? "-" : props.supplier_id}
+                </p>
+              </div>
+
+              <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
+                <div className="font-medium">Email</div>
+                <div className="font-medium">:</div>
+                <p className="font-medium">{props.email_supplier}</p>
+              </div>
+
+              <div className="grid w-[250px] grid-cols-[70px_6px_1fr] gap-1 text-[10px] text-black">
+                <div className="font-medium">No. Telpon</div>
+                <div className="font-medium">:</div>
+                <p className="font-medium">{props.no_telp}</p>
               </div>
             </div>
           </div>
         </div>
 
         <div className="grid gap-2">
-          <div className="grid grid-cols-[1fr_100px] gap-4">
-            <div className="grid gap-1 text-[10px]">
-              <p className="font-medium text-black">
-                Berikut adalah data item yang kami pesan :
-              </p>
-            </div>
-          </div>
+          <p className="text-[10px] font-medium text-black">
+            Berikut adalah data item yang kami pesan :
+          </p>
 
-          <Table
-            removeWrapper
-            isHeaderSticky
-            aria-label="preorder table"
-            classNames={{
-              base: ["max-h-[calc(100vh-100px)] overflow-scroll"],
-              table: ["border border-black"],
-              thead: [
-                "[&>tr]:first:rounded-none [&>tr]:first:shadow-none border-b border-black",
-              ],
-              th: [
-                "px-5 h-[14px] text-[10px] first:rounded-none last:rounded-none font-medium bg-transparent text-black",
-              ],
-              td: ["px-5 py-0 h-[14px]"],
-            }}
-            className="scrollbar-hide"
-          >
-            <TableHeader columns={columns}>
-              {(column) => (
-                <TableColumn key={column.uid}>{column.name}</TableColumn>
-              )}
-            </TableHeader>
+          <table className="table-auto border border-black">
+            <thead>
+              <tr className="border-b border-black text-left text-[10px] font-medium text-black">
+                <th className="px-2 py-1">No</th>
+                <th className="px-2 py-1">Kode Pabrik</th>
+                <th className="px-2 py-1">Nama Produk</th>
+                <th className="px-2 py-1">Qty</th>
+                <th className="px-2 py-1">Harga</th>
+                <th className="px-2 py-1">Jumlah</th>
+              </tr>
+            </thead>
 
-            <TableBody items={props.produk}>
-              {props.produk.map((item, index) => {
-                return (
-                  <TableRow key={item.nama_produk}>
-                    {(columnKey) => (
-                      <TableCell>
-                        {renderCell(index + 1, item, columnKey)}
-                      </TableCell>
-                    )}
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+            <tbody>
+              {props.produk.map((item, index) => (
+                <tr
+                  key={item.kode_pabrik}
+                  className="text-left text-[10px] text-black"
+                >
+                  <td className="w-[100px] px-2 py-1">{index + 1}</td>
+                  <td className="w-max px-2 py-1">
+                    {!item.kode_pabrik ? "-" : item.kode_pabrik}
+                  </td>
+                  <td className="w-max px-2 py-1">{item.nama_produk}</td>
+                  <td className="w-[100px] px-2 py-1">
+                    {item.qty} {item.satuan}
+                  </td>
+                  <td className="w-max px-2 py-1">
+                    {formatRupiah(item.harga)}
+                  </td>
+                  <td className="w-max px-2 py-1">
+                    {formatRupiah(item.jumlah)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <div className="flex items-center justify-between">
+
+        <div className="flex items-start justify-between">
           <div className="grid grid-cols-[45px_2px_1fr] gap-1 text-[10px] text-black">
             <div className="w-24 font-bold italic">Terbilang</div>
             <div className="font-bold italic">:</div>
@@ -197,6 +130,7 @@ const Preorder = (props: FinalDetail, ref: any) => {
               {angkaTerbilang(props.total)}
             </p>
           </div>
+
           <div className="grid w-[135px] border border-black p-2">
             <div className="grid grid-cols-[25px_6px_1fr] gap-1 text-[10px] text-black">
               <div className="w-24 font-medium">Total</div>
